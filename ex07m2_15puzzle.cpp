811e0e2aea956abea63491700d27e7a1c74755ae
Idhibhat Pankam <marginpankam@gmail.com>
2024-04-21 12:46:04 +0700

    a

--- ex07m2_15puzzle.cpp
+++ ex07m2_15puzzle.cpp
@@ -1,110 +1,110 @@
 #include<bits/stdc++.h>
 using namespace std;
-vector<vector<int> > board;
-vector<pair<int, int> > corPos = {{3,3},{0,0},{0,1},{0,2},{0,3},{1,0},{1,1},{1,2},{1,3},{2,0},{2,1},{2,2},{2,3},{3,0},{3,1},{3,2}};
-vector<int> reverseMove = {1, 0, 3, 2}; //0 up, 1 down, 2, left, 3 right
+vector<pair<int, int> > ord={{3,3},{0,0},{0,1},{0,2},{0,3},{1,0},{1,1},{1,2},{1,3},{2,0},{2,1},{2,2},{2,3},{3,0},{3,1},{3,2}};
+vector<int> reverseMoves = {1, 0, 3, 2};
 struct State {
-    int r;
-    int c;
-    vector<int> moves;
+    int r, c;
     vector<vector<int> > board;
+    vector<int> moves;
 
-    State(int r, int c, vector<int> moves, vector<vector<int> > board) : r(r), c(c), moves(moves), board(board) {}
-    State(const State &other) : r(other.r), c(other.c), moves(other.moves), board(other.board) {}
-    bool operator<(const State &other) const {
-        return moves.size() < other.moves.size();
-    }
+    State(int r, int c, vector<vector<int> > board, vector<int> moves) : r(r), c(c), board(board), moves(moves) {}
+    State(const State &other) : r(other.r), c(other.c), board(other.board), moves(other.moves) {}
     void print() {
-        cout << "-------------\n";
+        cout << "------------\n";
         for(int i=0;i<4;i++) {
             for(int j=0;j<4;j++) {
                 if(board[i][j] < 10) cout << " ";
                 cout << board[i][j] << " ";
-            } cout << "\n";
+            }
+            cout << "\n";
         }
-        cout << "moves = " << moves.size() << " : ";
+        cout << moves.size() << " moves: ";
         for(int i=0;i<moves.size();i++) cout << moves[i] << " ";
         cout << "\n";
     }
+
+    bool operator<(const State &other) const {
+        return moves.size() < other.moves.size();
+    }
 };
 
-bool canMove(State &s, int dir) {
-    if(dir == 0) { //up
+bool checkMove(State &s, int dir) { //0 up, 1 down, 2 left, 3 right
+    if(dir == 0) {
         if(s.r == 0) return false;
         swap(s.board[s.r][s.c], s.board[s.r-1][s.c]);
         s.r--;
-    } else if(dir == 1) { //down
+    } else if(dir == 1) {
         if(s.r == 3) return false;
         swap(s.board[s.r][s.c], s.board[s.r+1][s.c]);
         s.r++;
-    } else if(dir == 2) { //left
+    } else if(dir == 2) {
         if(s.c == 0) return false;
         swap(s.board[s.r][s.c], s.board[s.r][s.c-1]);
         s.c--;
-    } else if(dir == 3) { //right
+    } else if(dir == 3) {
         if(s.c == 3) return false;
         swap(s.board[s.r][s.c], s.board[s.r][s.c+1]);
         s.c++;
     }
     s.moves.push_back(dir);
-  
+
     return true;
 }
 
-int calHrt(State s) {
+int calHrt(State &s) {
     int sum = 0;
     for(int i=0;i<4;i++) {
         for(int j=0;j<4;j++) {
-            pair<int, int> goal = corPos[s.board[i][j]];
-            int errR = abs(goal.first - i);
-            int errC = abs(goal.second - j);
-            sum += errR + errC;
+            pair<int, int> corr = ord[s.board[i][j]];
+            int difr = abs(i - corr.first);
+            int difc = abs(j - corr.second);
+            sum += difr+difc;
         }
     }
+
     return sum + s.moves.size();
 }
 
 int main() {
-    pair<int, int> st;
-    board.resize(4);
+    State initState(0, 0, {}, {});
+    initState.board.resize(4);
     for(int i=0;i<4;i++) {
-        board[i].resize(4);
+        initState.board[i].resize(4);
         for(int j=0;j<4;j++) {
-            cin >> board[i][j];
-            if(board[i][j] == 0) st = {i, j};
+            cin >> initState.board[i][j];
+            if(initState.board[i][j] == 0) {
+                initState.r = i;
+                initState.c = j;
+            }
         }
     }
 
-    set<vector<vector<int> > > visited;
     priority_queue<pair<int, State> > pq;
-    State initState(st.first, st.second, {}, board);
-    pq.push({1, initState});
-    visited.insert(initState.board);
+    set<vector<vector<int> > > used;
     int ans = INT_MAX;
+    pq.push({-calHrt(initState), initState});
+    used.insert(initState.board);
     while(!pq.empty()) {
         int hrt = -pq.top().first;
         State s = pq.top().second;
-        int lastMove = s.moves.size() == 0 ? -1 : s.moves.back();
         pq.pop();
+        int lastMove = s.moves.size() == 0 ? -1 : s.moves.back();
+        // s.print();
+        // cout << "hrt = " << hrt << "\n";
 
         if(hrt == s.moves.size()) {
-            ans = min(ans, int(s.moves.size()));
+            ans = min(ans, hrt);
             break;
         }
-        // s.print();
 
         for(int i=0;i<4;i++) {
-            if(lastMove != -1 && reverseMove[lastMove] == i) continue;
+            if(lastMove != -1 && i == reverseMoves[lastMove]) continue;
             State ns(s);
-            // cout << "move " << i << "\n";
-            if(!canMove(ns, i)) {
-                // cout << "cannot move\n";
-                continue;
-            }
-            if(visited.find(ns.board) != visited.end()) continue;
-            int hrt = calHrt(ns);
-            pq.push({-hrt, ns});
-            visited.insert(ns.board);
+            if(!checkMove(ns, i)) continue;
+            if(used.find(ns.board) != used.end()) continue;
+            int nhrt = calHrt(ns);
+            pq.push({-nhrt, ns});
+            used.insert(ns.board);
         }
     }
 
